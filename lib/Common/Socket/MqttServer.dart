26365182902f0a0c245bc9faa394e9fa034c45f5
unknown <mortezasojodi70@gmail.com>
2021-09-07 11:12:02 +0430

    F

--- lib/Common/Socket/MqttServer.dart
+++ lib/Common/Socket/MqttServer.dart
@@ -14,43 +14,19 @@ class mttq {
       return client;
     }
 
-    /// A websocket URL must start with ws:// or wss:// or Dart will throw an exception, consult your websocket MQTT broker
-    /// for details.
-    /// To use websockets add the following lines -:
-    /// client.useWebSocket = true;
-    /// client.port = 80;  ( or whatever your WS port is)
-    /// There is also an alternate websocket implementation for specialist use, see useAlternateWebSocketImplementation
-    /// Note do not set the secure flag if you are using wss, the secure flags is for TCP sockets only.
-    /// You can also supply your own websocket protocol list or disable this feature using the websocketProtocols
-    /// setter, read the API docs for further details here, the vast majority of brokers will support the client default
-    /// list so in most cases you can ignore this.
-    /// Set logging on if needed, defaults to off
-    // client.logging(on: false);
     client.logging(on: false);
 
     /// If you intend to use a keep alive you must set it here otherwise keep alive will be disabled.
     client.keepAlivePeriod = 20;
 
-    /// Add the unsolicited disconnection callback
     client.onDisconnected = onDisconnected;
 
-    /// Add the successful connection callback
     client.onConnected = onConnected;
 
-    /// Add a subscribed callback, there is also an unsubscribed callback if you need it.
-    /// You can add these before connection or change them dynamically after connection if
-    /// you wish. There is also an onSubscribeFail callback for failed subscriptions, these
-    /// can fail either because you have tried to subscribe to an invalid topic or the broker
-    /// rejects the subscribe request.
     client.onSubscribed = onSubscribed;
 
-    /// Set a ping received callback if needed, called whenever a ping response(pong) is received
-    /// from the broker.
     client.pongCallback = pong;
 
-    /// Create a connection message to use or use the default one. The default one sets the
-    /// client identifier, any supplied username/password and clean session,
-    /// an example of a specific one below.
     var t = await localStorageGet("Token");
     final connMess = MqttConnectMessage()
         .authenticateAs('challenge', '8dAtPHvjPNC4erjFRfy')
@@ -58,12 +34,9 @@ class mttq {
         .withWillMessage('Will message')
         .startClean()
         .withWillQos(MqttQos.atLeastOnce);
-    print('EXAMPLE::Mosquitto client connecting....');
+
     client.connectionMessage = connMess;
 
-    /// Connect the client, any errors here are communicated by raising of the appropriate exception. Note
-    /// in some circumstances the broker will just disconnect us, see the spec about this, we however will
-    /// never send malformed messages.
     try {
       await client.connect();
     } on NoConnectionException catch (e) {
@@ -83,59 +56,8 @@ class mttq {
       /// Use status here rather than state if you also want the broker return code.
       print(
           'EXAMPLE::ERROR Mosquitto client connection failed - disconnecting, status is ${client.connectionStatus}');
-      // client.disconnect();
-      // exit(-1);
     }
 
-    /// The client has a change notifier object(see the Observable class) which we then listen to to get
-    /// notifications of published updates to each subscribed topic.
-    // client.updates!.listen((List<MqttReceivedMessage<MqttMessage?>>? c) {
-    //   final recMess = c![0].payload as MqttPublishMessage;
-    //   final pt =
-    //       MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
-
-    //   /// The above may seem a little convoluted for users only interested in the
-    //   /// payload, some users however may be interested in the received publish message,
-    //   /// lets not constrain ourselves yet until the package has been in the wild
-    //   /// for a while.
-    //   /// The payload is a byte buffer, this will be specific to the topic
-    //   print(
-    //       'EXAMPLE::Change notification:: topic is <${c[0].topic}>, payload is <-- $pt -->');
-    //   print('');
-    // });
-
-    /// If needed you can listen for published messages that have completed the publishing
-    /// handshake which is Qos dependant. Any message received on this stream has completed its
-    /// publishing handshake with the broker.
-
-    /// Lets publish to our topic
-    /// Use the payload builder rather than a raw buffer
-    /// Our known topic to publish to
-    // const pubTopic = 'Dart/Mqtt_client/testtopic';
-    // final builder = MqttClientPayloadBuilder();
-    // builder.addString('Hello from mqtt_client');
-
-    // /// Subscribe to it
-    // print('EXAMPLE::Subscribing to the Dart/Mqtt_client/testtopic topic');
-    // client.subscribe(pubTopic, MqttQos.exactlyOnce);
-
-    // /// Publish it
-    // print('EXAMPLE::Publishing our topic');
-    // client.publishMessage(pubTopic, MqttQos.exactlyOnce, builder.payload!);
-
-    /// Ok, we will now sleep a while, in this gap you will see ping request/response
-    /// messages being exchanged by the keep alive mechanism.
-    // print('EXAMPLE::Sleeping....');
-    // await MqttUtilities.asyncSleep(120);
-
-    // /// Finally, unsubscribe and exit gracefully
-    // print('EXAMPLE::Unsubscribing');
-    // client.unsubscribe(topic);
-
-    // /// Wait for the unsubscribe message from the broker if you wish.
-    // await MqttUtilities.asyncSleep(2);
-    // print('EXAMPLE::Disconnecting');
-    // client.disconnect();
     return client;
   }
 
--- lib/main.dart
+++ lib/main.dart
@@ -36,10 +36,6 @@ void main() {
   runApp(App());
 }
 
-/// A [StatelessWidget] which uses:
-/// * [bloc](https://pub.dev/packages/bloc)
-/// * [flutter_bloc](https://pub.dev/packages/flutter_bloc)
-/// to manage the state of a counter.
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -65,83 +61,3 @@ class App extends StatelessWidget {
     );
   }
 }
-
-// /// A [StatelessWidget] which demonstrates
-// /// how to consume and interact with a [CounterBloc].
-// class CounterPage extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(title: const Text('Counter')),
-//       body: BlocBuilder<CounterBloc, int>(
-//         builder: (_, count) {
-//           return Center(
-//             child: Text('$count', style: Theme.of(context).textTheme.headline1),
-//           );
-//         },
-//       ),
-//       floatingActionButton: Column(
-//         crossAxisAlignment: CrossAxisAlignment.end,
-//         mainAxisAlignment: MainAxisAlignment.end,
-//         children: <Widget>[
-//           Padding(
-//             padding: const EdgeInsets.symmetric(vertical: 5.0),
-//             child: FloatingActionButton(
-//               child: const Icon(Icons.add),
-//               onPressed: () =>
-//                   context.read<CounterBloc>().add(CounterEvent.increment),
-//             ),
-//           ),
-//           Padding(
-//             padding: const EdgeInsets.symmetric(vertical: 5.0),
-//             child: FloatingActionButton(
-//               child: const Icon(Icons.remove),
-//               onPressed: () =>
-//                   context.read<CounterBloc>().add(CounterEvent.decrement),
-//             ),
-//           ),
-//           Padding(
-//             padding: const EdgeInsets.symmetric(vertical: 5.0),
-//             child: FloatingActionButton(
-//               child: const Icon(Icons.brightness_6),
-//               onPressed: () => context.read<ThemeCubit>().toggleTheme(),
-//             ),
-//           ),
-//           Padding(
-//             padding: const EdgeInsets.symmetric(vertical: 5.0),
-//             child: FloatingActionButton(
-//               backgroundColor: Colors.red,
-//               child: const Icon(Icons.error),
-//               onPressed: () =>
-//                   context.read<CounterBloc>().add(CounterEvent.error),
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-
-
-// /// {@template counter_bloc}
-// /// A simple [Bloc] which manages an `int` as its state.
-// /// {@endtemplate}
-// class CounterBloc extends Bloc<CounterEvent, int> {
-//   /// {@macro counter_bloc}
-//   CounterBloc() : super(0);
-
-//   @override
-//   Stream<int> mapEventToState(CounterEvent event) async* {
-//     switch (event) {
-//       case CounterEvent.decrement:
-//         yield state - 1;
-//         break;
-//       case CounterEvent.increment:
-//         yield state + 1;
-//         break;
-//       case CounterEvent.error:
-//         addError(Exception('unsupported event'));
-//     }
-//   }
-// }
-
